e2ed941e4fb6c0db10b2329bea97bce4f3ebb76d
i7eo <admin@i7eo.com>
2023-09-22 18:40:56 +0800

    perf: change URI to fit REST API

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Inject, LoggerService, Patch, Post } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Inject, LoggerService, Param, Patch, Post } from '@nestjs/common'
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston'
 
 import { User } from './user.entity'
@@ -16,17 +16,13 @@ export class UserController {
   }
 
   @Post()
-  createUser(): any {
-    const user = {
-      username: 'i7eo',
-      password: '123456',
-    } as User
-
+  createUser(@Body() dto: any): any {
+    const user = dto as User
     return this.service.create(user)
   }
 
   @Get()
-  readUsers(): any {
+  getUsers(): any {
     this.logger.log('请求 /list 成功')
     this.logger.warn('请求 /list 成功')
     this.logger.verbose('请求 /list 成功')
@@ -35,43 +31,40 @@ export class UserController {
     return this.service.readList()
   }
 
-  @Get('/:id')
-  readUser(): any {
+  @Get(':id')
+  getUser(): any {
     // return this.service.read(username)
     return 'hello world'
   }
 
-  @Patch('/:id')
-  updateUser(): any {
-    const user = {
-      username: 'i7eo1',
-    } as User
-
-    return this.service.update(1, user)
+  @Patch(':id')
+  updateUser(@Param('id') id: number, @Body() dto: any): any {
+    const user = dto as User
+    return this.service.update(id, user)
   }
 
-  @Delete()
-  deleteUser(): any {
-    return this.service.delete(1)
+  @Delete(':id')
+  deleteUser(@Param('id') id: number): any {
+    return this.service.delete(id)
   }
 
-  @Get('profile')
-  readProfile(): any {
-    return this.service.readProfile(2)
+  @Get(':id/profile')
+  getUserProfile(@Param('id') id: number): any {
+    return this.service.readProfile(id)
   }
 
-  @Get('log')
-  readLog(): any {
-    return this.service.readLog(2)
+  @Get(':id/logs')
+  readLogs(@Param('id') id: number): any {
+    return this.service.readLog(id)
   }
 
-  @Get('logs')
-  readLogs(): any {
-    return this.service.readLogByGroup(2)
-  }
+  // @Get('logs')
+  // readLogs(): any {
+  //   return this.service.readLogByGroup(2)
+  // }
 
-  @Get('role')
-  readRole(): any {
-    return this.service.readRole(2)
+  @Get(':id/roles')
+  getUserRoles(@Param('id') id: number): any {
+    return this.service.readRole(id)
   }
 }
